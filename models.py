fd10404d293a4c41f32a0137c7a07f3d0c4e64f3
Harshal Kshirsagar <98384591+harshalplus1@users.noreply.github.com>
2023-08-07 23:21:50 +0530

    Update models.py

--- models.py
+++ models.py
@@ -111,7 +111,7 @@ class Generator(torch.nn.Module):
             x = xs / self.num_kernels
         x = F.leaky_relu(x)
         x = self.conv_post(x)
-        x = nn.SiLU(x)
+        x = F.SiLU(x)
 
         return x
 
