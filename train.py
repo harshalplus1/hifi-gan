4d0b3bab276505eeaec3786bdc46710566b3c7a1
Harshal Kshirsagar <98384591+harshalplus1@users.noreply.github.com>
2023-08-14 18:07:33 +0530

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import argparse
 import json
 import torch
 import torch.nn.functional as F
-from tensorboardX import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DistributedSampler, DataLoader
 import torch.multiprocessing as mp
 from torch.distributed import init_process_group
